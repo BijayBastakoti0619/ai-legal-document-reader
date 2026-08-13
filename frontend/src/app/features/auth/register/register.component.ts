ebc2a243fafa7eee6a7342aa6df22c2684295533
Bijay Bastakoti <bijaybastakoti111@gmail.com>
2026-08-13 14:07:38 -0500

    Made a flow to login when registration succeed.

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef,Component, inject } from '@angular/core';
 import {
   FormBuilder,
   ReactiveFormsModule,
@@ -13,6 +13,7 @@ import { RegisterRequest } from '../../../shared/models/register-request';
 import { ApiErrorResponse } from '../../../shared/models/api-error-response';
 import { passwordMatchValidator } from '../../../shared/validator/passwordMatchValidator';
 
+
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -24,6 +25,7 @@ export class RegisterComponent {
   private readonly formBuilder = inject(FormBuilder);
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private  readonly changeDetectorRef= inject(ChangeDetectorRef);
 
   isSubmitting = false;
   backendError = '';
@@ -89,6 +91,8 @@ export class RegisterComponent {
       .pipe(
         finalize(() => {
           this.isSubmitting = false;
+
+          this.changeDetectorRef.markForCheck();
         })
       )
       .subscribe({
@@ -104,6 +108,7 @@ export class RegisterComponent {
         },
         error: (error: HttpErrorResponse) => {
           this.handleBackendError(error);
+          this.changeDetectorRef.markForCheck();
         }
       });
   }

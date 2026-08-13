ecd6336a1f8acdfe619609b779dcdb284741b2b8
Bijay Bastakoti <bijaybastakoti111@gmail.com>
2026-08-13 14:07:38 -0500

    Made a flow to login when registration succeed.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register.component';
+import {LoginComponent} from './features/auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '',
